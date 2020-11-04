e506e660c96a30d06e23ae4147cb0413894964d2
mskian <veerfun23@gmail.com>
2020-11-04 10:02:39 +0530

    update pwa

--- assets/sw.js
+++ assets/sw.js
@@ -1,6 +1,4 @@
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwa-page";
 
